e22135319f0d150fc52fb50e05e2c61cec0cdd85
Jupiter-J <namju2912@gmail.com>
2022-05-05 15:28:50 +0900

    jwt toekn

---
+++ src/main/java/com/example/jwtsecurity/config/jwt/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.example.jwtsecurity.config.jwt;
+
+public interface JwtProperties {
+    String SECRET = "cos";  //우리 서버만 알고 있는 비밀값
+    int EXPIRATION_TIME = 1000 * 60 * 20;
+    String TOKEN_PREFIX = "Bearer ";
+    String HEADER_STRING = "Authorization";
+}
